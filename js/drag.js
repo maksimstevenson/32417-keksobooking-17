81fc8fcffad70d2f52a2b398a1c13515dc647a8f
Maksim Sianko <senkomaksim@gmail.com>
2019-07-22 19:25:35 -0700

    Modulation

---
+++ js/drag.js
@@ -0,0 +1,56 @@
+'use strict';
+
+window.data.mainPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var isDragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    isDragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var yCoordinate = window.data.mainPin.offsetTop - shift.y;
+    var xCoordinate = window.data.mainPin.offsetLeft - shift.x;
+
+    window.utils.setAdress(xCoordinate, yCoordinate);
+
+    if (yCoordinate < window.data.MAX_Y && yCoordinate > window.data.MIN_Y) {
+      window.data.mainPin.style.top = yCoordinate + 'px';
+    }
+
+    if (xCoordinate < window.data.MAX_X && xCoordinate > window.data.MIN_X) {
+      window.data.mainPin.style.left = xCoordinate + 'px';
+    }
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (isDragged) {
+      var onClickPreventDefault = function (deleteEvt) {
+        deleteEvt.preventDefault();
+        window.data.mainPin.removeEventListener('click', onClickPreventDefault);
+      };
+      window.data.mainPin.addEventListener('click', onClickPreventDefault);
+    }
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
---
+++ js/utils.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function () {
+  window.utils = {
+    // Function to get a random value in a specified range
+    getRandomNum: function (min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    // Function to get a random value from an array
+    getRandomValue: function (arr) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    },
+    // Function to get a random avatar picture
+    getLinkAvatar: function (index) {
+      return 'img/avatars/user' + 0 + (index + 1) + '.png';
+    },
+    // Function to get shuffled pictures
+    getShuffledPhotos: function (a) {
+      var j;
+      var x;
+      var i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = [x];
+      }
+      return a;
+    },
+    // Function to set address to input
+    setAdress: function (x, y) {
+      window.data.inputAdress.value = x + ',' + y;
+    },
+    // Function to create pins
+    renderPins: function (adverts) {
+      var template = document.querySelector('.map__pins'); // Переменная содержащая элемент в который добавляется пин
+      var mapPin = document.querySelector('#pin').content.querySelector('.map__pin'); // Переменная для обращения к шаблону пина
+      var fragment = document.createDocumentFragment(); // Фрагмент в котором хранится новый пин
+
+      adverts.forEach(function (item) { // Используем метод forEach для прочесывания каждого элмената массива и создания пина для каждого отдельного элемента
+        var pinElement = mapPin.cloneNode(true); // Копируем содержимое шаблона .map__pin и заносим в переменную для удобства использования
+        pinElement.querySelector('img').setAttribute('src', item.author.avatar); // обращаемся к содержимому и назначаем атрибут
+        pinElement.setAttribute('style', 'left: ' + item.location.x + 'px; ' + 'top: ' + item.location.y + 'px');// Задаем позицию пина через атрибут
+        fragment.appendChild(pinElement); // С помощью метода appendChild назначаю данные фрагменту
+        template.appendChild(fragment); // Используя тот же метод назначаю фрагмент с заданными параметрами в template
+      });
+    }
+  };
+})();
